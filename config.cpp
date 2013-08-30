613b980b4adc77f7da0c31167fc2617527fa3dd9
Ray Fung <posixfung@163.com>
2013-08-30 15:39:08 +0800

    add Config::readConfigFromFile() and so on

--- config.cpp
+++ config.cpp
@@ -1,5 +1,50 @@
 #include "config.h"
+#include <QFile>
+#include <QTextStream>
 
 Config::Config()
 {
+    bindAddress = QString("0.0.0.0");
+    bindPort    = 9866;
+    dbFilePath  = QString("./im.db");
+}
+
+QString Config::getIP()
+{
+    return bindAddress;
+}
+
+quint16 Config::getPort()
+{
+    return bindPort;
+}
+
+QString Config::getDBPath()
+{
+    return dbFilePath;
+}
+
+bool Config::readConfigFromFile(QString path)
+{
+    QFile configFile(path);
+    if(!configFile.open(QFile::ReadOnly))
+        return false;
+
+    QTextStream in(&configFile);
+    QString ip, port, db;
+
+    ip = in.readLine();
+    if(ip.isEmpty())
+        return false;
+    port = in.readLine();
+    if(port.isEmpty())
+        return false;
+    db = in.readLine();
+    if(db.isEmpty())
+        return false;
+
+    bindAddress = ip;
+    bindPort    = port.toUInt();
+    dbFilePath  = db;
+    return true;
 }
--- config.h
+++ config.h
@@ -1,10 +1,21 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <QString>
+
 class Config
 {
 public:
     Config();
+    bool readConfigFromFile(QString path);
+    QString getIP();
+    quint16 getPort();
+    QString getDBPath();
+
+private:
+    QString bindAddress;
+    quint16 bindPort;
+    QString dbFilePath;
 };
 
 #endif // CONFIG_H
